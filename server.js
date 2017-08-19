06c7a924b09965e345a90ccc8cc15104dcda03e7
rahulranjan72 <rahul.ranjan72@hotmail.com>
2017-08-20 01:30:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function(req,res){
-    res.send("Article one File is Requested and is going to serve");
+    res.sendFile(path.join(_dirname,'ui',article-one.html));
 });
 app.get('/article-two', function(req,res){
     res.send("Article two File is Requested and is going to serve");
